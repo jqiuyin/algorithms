db23e4a8ea85d2cfe5c8e4534b0408c5f6446b10
jqiuyin <943571574@qq.com>
2018-05-10 22:50:34 +0800

    尝试写一些注释

--- src/thirdUnit/LinearProbingHashST.java
+++ src/thirdUnit/LinearProbingHashST.java
@@ -14,12 +14,27 @@ public class LinearProbingHashST<Key,Value> {
 		keys=(Key[]) new Object[M];
 		vals=(Value[]) new Object[M];
 	}
+	public LinearProbingHashST(int M) {
+		this.M=M;
+		// TODO 自动生成的构造函数存根
+		keys=(Key[]) new Object[M];
+		vals=(Value[]) new Object[M];
+	}
 	private int hash(Key key) {
 		return (key.hashCode()&0x7fffffff);
 		
 	}
 	private void resize(int cap) {
-		
+		LinearProbingHashST<Key, Value> t;
+		t=new LinearProbingHashST<>(cap);
+		for(int i=0;i<M;i++) {
+			if(keys[i]!=null) {
+				t.put(keys[i], vals[i]);
+			}
+		}
+		keys=t.keys;
+		vals=t.vals;
+		M=t.M;
 	}
 	public void put(Key key,Value val) {
 		if(N>=M/2)resize(2*M);//动态扩展数组大小
@@ -42,4 +57,30 @@ public class LinearProbingHashST<Key,Value> {
 		}
 		return null;
 	}
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+	public void delete(Key key) {
+		if(!contains(key))return;
+		int i=hash(key);
+		while(!key.equals(keys[i])) {
+			i=(i+1)%M;
+		}
+		keys[i]=null;
+		vals[i]=null;
+		i=(i+1)%M;
+		while(keys[i]!=null) {
+			Key keyToRedo=keys[i];
+			Value valToRedo=vals[i];
+			keys[i]=null;
+			vals[i]=null;
+			N--;
+			put(keyToRedo, valToRedo);
+			i=(i+1)%M;
+		}
+		N--;
+		if(N>0&&N<=M/8)resize(M/2);
+	}
+	
+	
 }
