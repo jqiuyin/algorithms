cafecad5387a108d3c0256c592931af5ed29ae11
jqiuyin <943571574@qq.com>
2018-05-21 00:22:06 +0800

    判断无环图

---
+++ src/forthUnit/Cycle.java
@@ -0,0 +1,29 @@
+package forthUnit;
+
+public class Cycle {
+
+	private boolean[] marked;
+	private boolean hasCycle;
+	public Cycle(Graph G) {
+		// TODO 自动生成的构造函数存根
+		for(int s=0;s<G.V();s++) {
+			if(!marked[s]) {
+				dfs(G, s, s);
+			}
+		}
+	}
+	private void dfs(Graph G,int v,int u) {
+		marked[v]=true;
+		for(int w:G.adj(v)) {
+			if(!marked[w]) {
+				dfs(G, w, v);
+			}
+			else if(w!=u) {
+				hasCycle=true;
+			}
+		}
+	}
+	public  boolean hasCycle() {
+		return hasCycle;
+	}
+}
